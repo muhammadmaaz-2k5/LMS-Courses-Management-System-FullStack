0d08f90187ca416f35ae25619289a9a966fa90a5
muhammadmaaz2k5 <22pwbcs0933@uetpeshawar.edu.pk>
2026-07-26 17:55:08 +0500

    fix: add missing assets import in educator Navbar

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { dummyEducatorData } from "../../assets/assets";
+import { assets, dummyEducatorData } from "../../assets/assets";
 import { UserButton, useUser } from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
 import Logger from "../Logger";
